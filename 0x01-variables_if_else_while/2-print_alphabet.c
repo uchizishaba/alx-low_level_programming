34559c749cd2a3ac308aa0c22b391c579e100163
uchizi shaba <uchizishaba@gmail.com>
2023-02-19 01:19:41 -0800

    message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Default function
+ *   *
+ *    * Return: Returns a 1 (Success)
+ *     */
+int main(void)
+{
+		char letter;
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+						putchar(letter);
+
+				putchar('\n');
+
+					return (0);
+}
