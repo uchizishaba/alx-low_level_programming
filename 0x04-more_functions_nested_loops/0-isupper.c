5c5d28addaf165070865ea0935298cd29abfcefe
uchizi shaba <uchizishaba@gmail.com>
2023-02-23 01:50:17 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _isupper - Check if a character is upper
+ * @c: the number to be checked
+ * Return : 1 for upper ltter or o for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 98)
+	{
+	return (1);
+	}
+	return (0);
+
+}
+
