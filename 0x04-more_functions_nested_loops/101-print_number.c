1466c7662107dc6fe5ab30898aa5e023aeb24511
uchizi shaba <uchizishaba@gmail.com>
2023-02-23 12:14:06 -0800

    correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 void print_integer(int m);
 
 /**
@@ -6,6 +7,7 @@ void print_integer(int m);
  * @n: An input integer
  * Return: Nothing
  */
+
 void print_number(int n)
 {
 	if (n == 0)
@@ -24,6 +26,7 @@ void print_number(int n)
  * @m: an input unsigned integer
  * Return: Nothing
  */
+
 void print_integer(int m)
 {
 	int i = 1000000000;
