f56801e49ad0f34119a42072f4acc5962e4e7972
uchizi shaba <uchizishaba@gmail.com>
2023-03-06 02:26:34 -0800

    strspn correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,32 @@
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * _strpbrk - searchs a string for any of a set of bytes
- *
- * @s: the string to search
- * @accept: the bytes to search for
- *
- * Return: a pointer to the byte in @s that matches one of the bytes in
- * @accept, or NULL if no such byte is found.
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
-
-char *_strpbrk(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	unsigned int i, n, value, check;
 
-	while (*s)
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i]; i++)
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (*s == accept[i])
-				return (s);
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
 		}
-		s++;
+		if (check == 0)
+			return (value);
 	}
-	return (NULL);
+
+	return (value);
 }
