174c52107260150f0e8fae7548ce8f8af80c0b5b
uchizi shaba <uchizishaba@gmail.com>
2023-04-11 14:26:10 -0700

    correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,10 +11,11 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int number_of_bits;
+
 	for (number_of_bits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
-			nnumber_of_bits++;
+			number_of_bits++;
 	}
 	return (number_of_bits);
 }
