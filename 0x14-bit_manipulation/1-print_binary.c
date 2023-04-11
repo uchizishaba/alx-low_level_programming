4b8556382e5d318faf363e0007ffa724a1f1a344
uchizi shaba <uchizishaba@gmail.com>
2023-04-11 14:37:22 -0700

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,9 +6,11 @@
  *
  * @param n The number to convert into binary representation.
  */
-void print_binary(unsigned long int n) {
-    if (n > 1) {
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
         print_binary(n >> 1);
-    }
-    _putchar((n & 1) + '0');
+	}
+	 _putchar((n & 1) + '0');
 }
