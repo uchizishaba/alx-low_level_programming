90bd1fa65adb2daab8533bc8ec061ab3ea71aedd
uchizi shaba <uchizishaba@gmail.com>
2023-04-11 08:46:00 -0700

    convert unasigned int to base 2

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - the function converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int, the comverted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int unint;
+	int len, base_2;
+
+	if (!b)
+		return (0);
+
+	unint = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_2 = 1; len >= 0; len--,base_2 *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			unint += base_2;
+		}
+	}
+
+	return (unint);
+}i
