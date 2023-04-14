cc8d479346e4a738799365bb7e59b3f695c4f68f
uchizi shaba <uchizishaba@gmail.com>
2023-04-14 13:38:20 -0700

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 	{
 		return (0);
-	}	
+	}
 	open_file = open(filename, O_RDONLY);
 	read_file = read(open_file, buff, letters);
 	write_file = write(STDOUT_FILENO, buff, read_file);
