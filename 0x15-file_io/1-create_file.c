90d47ccab218dcd678433cb4d255f741bc65de16
uchizi shaba <uchizishaba@gmail.com>
2023-04-14 14:04:24 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,10 +8,9 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int rwx;
+	int rwr;
 	int fid;
-	int nletters;
-
+	int nlet;
 
 	if (!filename)
 		return (-1);
@@ -24,10 +23,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for ( nlet = 0; text_content[nlet]; nlet++)
 		;
 
-	rwx = write(fid, text_content, nletters);
+	rwr = write(fid, text_content, nlet);
 
 	if (rwr == -1)
 		return (-1);
